88b541dc45034bcb241ec8e0d030daaf79393503
hughfdjackson <hughfdjackson@googlemail.com>
2017-12-14 15:43:50 +0000

    reset learning rate, and use even lower training numbers

--- models/model.py
+++ models/model.py
@@ -199,7 +199,7 @@ def _model_checkpoint(file_path):
         save_best_only = True
     )
 
-def _learning_rate_scheduler(step = 15, initial_learning_rate = 2e-3, minimum_learning_rate = 2e-5):
+def _learning_rate_scheduler(step = 15, initial_learning_rate = 1e-3, minimum_learning_rate = 1e-5):
     def anneal(epoch):
         learning_rate = initial_learning_rate / (10 ** (epoch // step))
 
