cefbc32b7bf433bb8dff9b0f35b61fc784284ec5
hughfdjackson <hughfdjackson@googlemail.com>
2018-01-02 15:22:43 +0000

    load model from disk

--- build/__main__.py
+++ build/__main__.py
@@ -8,4 +8,5 @@ from build.train_and_test import train_only
 from models.model import load
 
 if __name__ == '__main__':
+    model = load('builds/FCRN_Peak_Mask_B-fcrn_b/3/FCRN_Peak_Mask_B-fcrn_b.hdf5')
     test_with_increasing_curve(model, number_of_sigmas = 6)
